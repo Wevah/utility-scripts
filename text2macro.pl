6b555b7cc7871b5b8352f185750831af6ac34f08
Nate Weaver <wevah@derailer.org>
2021-06-29 14:08:18 -0500

    Add `style` option

--- text2macro.pl
+++ text2macro.pl
@@ -5,20 +5,29 @@ use warnings;
 use Getopt::Long;
 
 my $stripblanks = 0;
+my $style = "objc";
 
-GetOptions('stripblanks' => \$stripblanks);
+GetOptions('stripblanks' => \$stripblanks, 'style=s' => \$style);
 
-my $file = $ARGV[0] or die("I need a file.\n");
-my $macroname = $ARGV[1] or die("I need a macro name.\n");
+my ($file, $macroname) = @ARGV or die <<'USAGE';
+Usage: text2macro.pl [--style=<style>] [--stripblanks] <file> <macro name>
+USAGE
 
 open(my $fh, "<", $file) or die ("Couldn't read '$file'.\n");
 
 print "#define $macroname ";
+
+my ($start, $end);
+
+if ($style eq "objc") { $start = '@"'; $end = '"' }
+elsif ($style eq "c") { $start = '"'; $end = '"' }
+else { die "Unknown style value.\n" }
 
 while (my $line = <$fh>) {
 	next if $stripblanks && $line =~ m/^\s+$/;
 	
 	$line =~ s/(")/\\$1/g;
 	$line =~ s/\n/\\n/g;
-	print qq/@"$line" \\\n/;
+	
+	print qq/$start$line$end\\\n/;
 }
